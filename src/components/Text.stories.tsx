bd40076b15964e52c4524e088d6d222da38b3e02
Paulo Santana <paulo.santana@lovys.com>
2022-10-17 17:41:39 -0300

    Initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: "Components/Text",
+  component: Text,
+  args: {
+    children: "Lorem ipsum text",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: "sm",
+  },
+};
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+  },
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: <p>Text with P tag</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      disable: true,
+    },
+  },
+};
